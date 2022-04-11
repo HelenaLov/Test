8a3c5c8e8e5df5b2614fb51ecdeaa42195b5921e
unknown <olenalevychkina@gmail.com>
2022-04-11 11:19:39 +0300

    second commit

--- src/test/java/MyTest/TestArray.java
+++ src/test/java/MyTest/TestArray.java
@@ -3,6 +3,8 @@ package MyTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 
 public class TestArray {
     @Test
@@ -36,5 +38,26 @@ public class TestArray {
     }
 
     private void extracted(boolean b) {
+    }
+
+    @Test
+    public void intEqualsIntVsIntegerAssertTrue() {
+        Assert.assertEquals(4, 4);
+        Assert.assertTrue(4 == 4);
+
+        Integer firstFour = new Integer(4);
+        Integer secondFour = new Integer(4);
+
+        Assert.assertEquals(firstFour, secondFour);
+        Assert.assertTrue(firstFour.equals(secondFour));
+    }
+
+    @Test
+    public void canConfirmIntMinAndMaxLimits() {
+        int minimumInt = -2147483648;
+        int maximumInt = 2147483647;
+
+        assertEquals("integer min", minimumInt, Integer.MIN_VALUE);
+        assertEquals("integer max", maximumInt, Integer.MAX_VALUE);
     }
 }
